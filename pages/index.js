3f66d3a0fc65c61ef9bede7bc75bc490d39a344e
Matheus Cassarin <matheuscassarin@gmail.com>
2026-06-15 23:48:46 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Sua falta de fé é perturbadora</h1>
+    return <h1>I would rather share one lifetime with you than face all the ages of this world alone </h1>
 }
 
 export default Home;
